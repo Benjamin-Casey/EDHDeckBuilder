4ead4c746ab734af7d49ea0c837a86a181bc588a
Benjamin Casey <benjamincasey55@gmail.com>
2021-03-16 03:43:16 +1000

    Implemented starting classes (testing at bottom)

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
 """
-EDH Magic the Gathering Deck Builder
-
-Author: Benjamin Casey
-Requirements: Scrython
+Next: save and reopen decks.
 """
 
-def find_card(card_name):
-    pass
+class DeckBox:
+    def __init__(self, **kwargs):
+        self.decks = kwargs.get('decks', [])
 
+
+class Deck:
+    def __init__(self, name, commander, **kwargs):
+        self.name = name
+        self.commander = commander
+        self.groups = kwargs.get('groups', [Group("Nonlands", []), Group("Lands", [])])       # List of groups
+
+    def add_card(self, card, group_name):
+        for group in self.groups:
+            if group.name == group_name:
+                group.cards.append(card)
+                break
+            else:
+                raise Exception("There is no such group in deck")
+
+
+class Group:
+    def __init__(self, name, cards):
+        self.name = name       
+        self.cards = cards      # List of Cards
+
+
+class Card:
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.qty = kwargs.get('qty', 1)
+
+
+class Menu:
+    items = []
+
+    def print_menu(self):
+        statement = "----------  Menu  ----------\n\n"
+        for item in self.items:
+            statement += " - {}\n".format(item)
+        statement += "\n----------------------------"
+        print(statement)
+
+
+
+
+# Test items
+
+test_deck = Deck("Orvar Wizards", Card("Orvar, the All-form"))
+
+main_menu = Menu()
+main_menu.items.append("Make new deck")
+main_menu.print_menu()
